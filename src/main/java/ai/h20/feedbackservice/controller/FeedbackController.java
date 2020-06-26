d37ae168f541a8ddf3f8b11c175d12082ae8b621
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2020-06-26 18:51:19 +0530

    Enable CORS

--- src/main/java/ai/h20/feedbackservice/controller/FeedbackController.java
+++ src/main/java/ai/h20/feedbackservice/controller/FeedbackController.java
@@ -6,6 +6,7 @@ import ai.h20.feedbackservice.service.FeedbackService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "/feedback")
 public class FeedbackController {
