9ecf82e36f65eec3254789009430a9798f8ee302
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2020-06-28 09:46:56 +0530

    Added sonar integration

--- src/test/java/ai/h2o/feedback/utils/RequestUtilsTest.java
+++ src/test/java/ai/h2o/feedback/utils/RequestUtilsTest.java
@@ -1,42 +1,43 @@
 package ai.h2o.feedback.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.data.domain.Pageable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class RequestUtilsTest {
+public class RequestUtilsTest {
 
     @Test
-    void testGetPageableForSizeParam() {
+    public void testGetPageableForSizeParam() {
         Pageable pageable = RequestUtils.getPageable(-1, 5);
-        assertEquals(0, pageable.getPageNumber(), "Page no should be reset to 0 for all values < 1");
+        assertEquals("Page no should be reset to 0 for all values < 1", 0, pageable.getPageNumber());
 
         pageable = RequestUtils.getPageable(0, 5);
-        assertEquals(0, pageable.getPageNumber(), "Page no should be reset to 0 for all values < 1");
+        assertEquals("Page no should be reset to 0 for all values < 1", 0, pageable.getPageNumber() );
 
         pageable = RequestUtils.getPageable(1, 5);
-        assertEquals(0, pageable.getPageNumber(), "Returned page no should be 1 less than passed page no");
+        assertEquals("Returned page no should be 1 less than passed page no",0, pageable.getPageNumber());
 
         pageable = RequestUtils.getPageable(2, 5);
-        assertEquals(1, pageable.getPageNumber(), "Returned page no should be 1 less than passed page no");
+        assertEquals("Returned page no should be 1 less than passed page no",1, pageable.getPageNumber());
     }
 
     @Test
-    void testGetPageableForPageParam(){
+    public void testGetPageableForPageParam(){
         Pageable pageable = RequestUtils.getPageable(2, -1);
-        assertEquals(1, pageable.getPageSize(), "Size should be reset to 1 for all values < 1");
+        assertEquals("Size should be reset to 1 for all values < 1",1, pageable.getPageSize());
 
         pageable = RequestUtils.getPageable(2, 0);
-        assertEquals(1, pageable.getPageSize(), "Size should be reset to 1 for all values < 1");
+        assertEquals("Size should be reset to 1 for all values < 1",1, pageable.getPageSize());
 
         pageable = RequestUtils.getPageable(2, 1);
-        assertEquals(1, pageable.getPageSize(), "All positive nos less than or equal to 1000 are valid as page size");
+        assertEquals("All positive <= 1000 are valid as page size",1, pageable.getPageSize());
 
         pageable = RequestUtils.getPageable(2, 1000);
-        assertEquals(1000, pageable.getPageSize(), "All positive nos less than or equal to 1000 are valid as page size");
+        assertEquals("All positive nos <= 1000 are valid as page size",1000, pageable.getPageSize());
 
         pageable = RequestUtils.getPageable(2, 1001);
-        assertEquals(1000, pageable.getPageSize(), "Size should be reset to 1000 for all values > 1000");
+        assertEquals( "Size should be reset to 1000 for all values > 1000", 1000, pageable.getPageSize());
     }
+
 }
